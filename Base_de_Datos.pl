13693d92e57977ed085169539214a965b87a4340
brema026 <Nif_Zawtx@outlook.com>
2025-05-05 13:48:03 -0600

    Aeropuertos y vuelos usando BNF

--- Base_de_Datos.pl
+++ Base_de_Datos.pl
@@ -1,19 +1,38 @@
-% Aeropuertos
-aeropuerto(sjo, 'San José, Costa Rica').
-aeropuerto(pty, 'Ciudad de Panamá, Panamá').
-aeropuerto(jfk, 'New York, Estados Unidos').
-aeropuerto(phx, 'Phoenix, Arizona').
-aeropuerto(lax, 'Los Ángeles, California').
+<datos> ::= <aeropuertos> <vuelos>
 
-% Vuelos
-% vuelo(Codigo, Origen, Destino, Aerolinea, Clase, DuracionHoras, CostoUSD)
-vuelo(cm404, sjo, pty, 'Copa Airlines', negocios, 1, 320).
-vuelo(ua105, pty, phx, 'United Airlines', economica, 6, 580).
-vuelo(aa300, sjo, jfk, 'American Airlines', economica, 5, 400).
-vuelo(dl200, jfk, lax, 'Delta Airlines', negocios, 6, 900).
-vuelo(ua777, lax, phx, 'United Airlines', economica, 1, 200).
-vuelo(cm500, pty, lax, 'Copa Airlines', economica, 5, 450).
-vuelo(ua900, sjo, phx, 'United Airlines', negocios, 7, 1000).
-vuelo(aa111, phx, jfk, 'American Airlines', economica, 5, 380).
-vuelo(dl300, jfk, pty, 'Delta Airlines', negocios, 6, 850).
-vuelo(la250, sjo, lax, 'LATAM Airlines', economica, 6, 420).
+<aeropuertos> ::= <aeropuerto> | <aeropuerto> <aeropuertos>
+
+<aeropuerto> ::= "aeropuerto(" <codigo_aeropuerto> "," <nombre_aeropuerto> ")"
+
+<codigo_aeropuerto> ::= "sjo" | "pty" | "jfk" | "phx" | "lax"
+
+<nombre_aeropuerto> ::= "'San José, Costa Rica'"
+                     | "'Ciudad de Panamá, Panamá'"
+                     | "'New York, Estados Unidos'"
+                     | "'Phoenix, Arizona'"
+                     | "'Los Ángeles, California'"
+
+<vuelos> ::= <vuelo> | <vuelo> <vuelos>
+
+<vuelo> ::= "vuelo(" <codigo> "," <origen> "," <destino> "," <aerolinea> "," <clase> "," <duracion> "," <precio> ")"
+
+<codigo> ::= "cm101" | "cm102" | "ua103" | "aa104" | "dl105" | "la106" | "ua107" | "cm108" | "aa109" | "dl110"
+           | "cm201" | "cm202" | "ua203" | "aa204" | "dl205" | "la206" | "ua207" | "cm208" | "aa209" | "dl210"
+           | "cm301" | "cm302" | "ua303" | "aa304" | "dl305" | "la306" | "ua307" | "cm308" | "aa309" | "dl310"
+           | "cm401" | "cm402" | "ua403" | "aa404" | "dl405" | "la406" | "ua407" | "cm408" | "aa409" | "dl410"
+           | "cm501" | "cm502" | "ua503" | "aa504" | "dl505" | "la506" | "ua507" | "cm508" | "aa509" | "dl510"
+
+<origen> ::= "sjo" | "pty" | "jfk" | "phx" | "lax"
+
+<destino> ::= "sjo" | "pty" | "jfk" | "phx" | "lax"
+
+<aerolinea> ::= "'Copa Airlines'" | "'United Airlines'" | "'American Airlines'" | "'Delta Airlines'" | "'LATAM Airlines'"
+
+<clase> ::= "economica" | "negocios"
+
+<duracion> ::= "1" | "5" | "6" | "7"
+
+<precio> ::= "200" | "210" | "220" | "240" | "250" | "300" | "310" | "400" | "410" | "420"
+           | "430" | "500" | "510" | "520" | "530" | "540" | "550" | "560" | "580" | "590"
+           | "600" | "610" | "620" | "640" | "650" | "660" | "670" | "680" | "690" | "700"
+           | "710" | "720" | "750"
