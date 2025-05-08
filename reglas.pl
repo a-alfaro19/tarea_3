a5eaecf5c6588fc15a2fd0acdca7e35b4714780a
melivasr <anavasquez@estudiantec.cr>
2025-05-08 05:27:51 -0600

    Falta documentación interna

--- reglas.pl
+++ reglas.pl
@@ -8,6 +8,8 @@ sintagma_nominal(origen(Origen), vacio, vacio, vacio, vacio, vacio) -->
     skip, [estoy, en], skip, lugar(Origen), skip.
 sintagma_nominal(origen(Origen), vacio, vacio, vacio, vacio, vacio) -->
     skip, lugar(Origen), skip.
+sintagma_nominal(origen(Origen), vacio, vacio, vacio, vacio, vacio) -->
+    skip, [salgo, de], skip, lugar(Origen), skip.
 
 % ----------------------------
 % Sintagma nominal para DESTINO
@@ -19,6 +21,10 @@ sintagma_nominal(vacio, destino(Destino), vacio, vacio, vacio, vacio) -->
     skip, [voy, a], skip, lugar(Destino), skip.
 sintagma_nominal(vacio, destino(Destino), vacio, vacio, vacio, vacio) -->
     skip, [hasta], skip, lugar(Destino), skip.
+sintagma_nominal(vacio, destino(Destino), vacio, vacio, vacio, vacio) -->
+    skip, [me, gustaria, ir, a], skip, lugar(Destino), skip.
+sintagma_nominal(vacio, destino(Destino), vacio, vacio, vacio, vacio) -->
+    skip, [ir, a], skip, lugar(Destino), skip.
 sintagma_nominal(vacio, destino(Destino), vacio, vacio, vacio, vacio) -->
     skip, lugar(Destino), skip.
 
@@ -54,8 +60,40 @@ sintagma_nominal(vacio, vacio, vacio, vacio, clase(Clase), vacio) -->
 sintagma_nominal(vacio, vacio, vacio, vacio, vacio, presupuesto(Pres)) -->
     skip, presupuesto_opc(Pres), skip.
 
+% =======================================
+% SINTAGMAS VERBALES
+% =======================================
+
+sintagma_verbal(deseo_viajar) -->
+    skip, [quiero, viajar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [deseo, volar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [me, gustaria, volar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [me, gustaria, ir], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [me, gustaria, viajar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [planeo, viajar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [planeo, ir], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [necesito, viajar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [quisiera, viajar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [quisiera, ir], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [tengo, que, viajar], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [tengo, que, ir], skip.
+sintagma_verbal(deseo_viajar) -->
+    skip, [salida], skip.
+
+
 % ----------------------------
-% Lugares (con códigos oficiales)
+% LUGARES (sinónimos + más variantes)
 % ----------------------------
 
 % SJO - San Jose, Costa Rica
@@ -95,30 +133,38 @@ lugar(lax) --> [california].
 tipo_vuelo(charter) --> [charter].
 tipo_vuelo(comercial) --> [comercial].
 tipo_vuelo(directo) --> [directo].
+tipo_vuelo(barato) --> [barato].
 
 % ----------------------------
-% Aerolíneas
+% Aerolíneas (sinónimos o variantes)
 % ----------------------------
 
 aerolinea_opc(copa_airlines) --> [copa, airlines].
+aerolinea_opc(copa_airlines) --> [copa].
 aerolinea_opc(avianca) --> [avianca].
 aerolinea_opc(united_airlines) --> [united, airlines].
+aerolinea_opc(united_airlines) --> [united].
 aerolinea_opc(american_airlines) --> [american, airlines].
+aerolinea_opc(american_airlines) --> [american].
 aerolinea_opc(delta_airlines) --> [delta, airlines].
+aerolinea_opc(delta_airlines) --> [delta].
 
 % ----------------------------
-% Clase
+% Clase (sinónimos o variantes)
 % ----------------------------
 
 clase_opc(economica) --> [economica].
 clase_opc(economica) --> [económica].
+clase_opc(economica) --> [economy].
 clase_opc(negocios) --> [negocios].
+clase_opc(negocios) --> [business].
 
 % ----------------------------
 % Presupuesto
 % ----------------------------
 
 presupuesto_opc(Monto) --> [Monto, dolares], { number(Monto) }.
+presupuesto_opc(Monto) --> [Monto], { number(Monto) }.
 
 % ----------------------------
 % Skip: permite saltar cualquier palabra innecesaria antes/después
